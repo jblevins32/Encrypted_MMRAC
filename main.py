1c609e4851130d27d0ae3f6fb5869ce910aaeff1
Jacob <jacob.blevins@gatech.edu>
2023-06-18 10:22:39 -0400

    figured out how encoding is affecting beta. Could be a big issue.

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from dyers import *
 from dyersmat import *
+import math
+
+# reg_vec isn't giving correct output because beta 0 is losing data to encoding
+# enc_c is erroring
 
 # Encryption
 bit_length = 256
@@ -40,7 +44,7 @@ def main():
     Dr = np.zeros((2, 1))
 
     # Initial Conditions
-    x = np.array([[0], [0]])
+    x = np.array([[0], [1]])
     xr = np.array([[0], [0]])
 
     # Other variables
@@ -52,45 +56,66 @@ def main():
     r = 0
     Ts = 0.2
     par_dot_vec = []
-    x_vec = []
-    xr_vec = []
+    enc_x_vec = []
+    enc_xr_vec = []
+    enc_e_vec = []
     e_vec = []
-    reg_vec = []
+    enc_reg_vec = []
     u_vec = []
     par_vec = []
     par = np.array([[0], [0]])
 
     # encryption of matricies and variables
-    enc_A = mat_enc(A, kappa, p, modulus, delta)
-    enc_B = mat_enc(B, kappa, p, modulus, delta)
-    enc_C = mat_enc(C, kappa, p, modulus, delta)
-    enc_D = mat_enc(D, kappa, p, modulus, delta)
+    enc_d = enc(1/delta, kappa, p, modulus, delta) # for balancing encoding depths
     enc_Ar = mat_enc(Ar, kappa, p, modulus, delta)
     enc_Br = mat_enc(Br, kappa, p, modulus, delta)
-    enc_Cr = mat_enc(Cr, kappa, p, modulus, delta)
-    enc_Dr = mat_enc(Dr, kappa, p, modulus, delta)
+    enc_A = mat_enc(A, kappa, p, modulus, delta)
+    enc_B = mat_enc(B, kappa, p, modulus, delta)
+    enc_xr = mat_enc(xr, kappa, p, modulus, delta)
     enc_x = mat_enc(x, kappa, p, modulus, delta)
     enc_r = enc(r, kappa, p, modulus, delta)
-    enc_xr = mat_enc(xr, kappa, p, modulus, delta)
+    enc_u = enc(u, kappa, p, modulus, delta)
+    #enc_c = mat_enc(c, kappa, p, modulus, delta)
+    enc_beta_0 = enc(beta_0, kappa, p, modulus, delta)
+    enc_beta_1 = enc(beta_1, kappa, p, modulus, delta)
+    enc_r_dot = enc(r_dot, kappa, p, modulus, delta)
+    enc_r_ddot = enc(r_ddot, kappa, p, modulus, delta)
 
-    mult_test = mat_dec(np.dot(enc_Ar, enc_xr), kappa, p, delta)
-
-    print(mult_test)
-
-    k = 0  # time step
+    k = 0  # step
+    t = 0  # time
     while k <= 50:
-        xr = np.dot(enc_Ar, enc_xr) + np.dot(enc_Br, enc_r)
-        x = np.dot(A, x) + np.dot(B, u)
-        x_vec.append(x.flatten())
-        xr_vec.append(xr.flatten())
-        e = x - xr
-        e_vec.append(e.flatten())
-        eps = np.dot(c, e)
+        enc_xr = np.dot(enc_Ar, enc_xr) + np.dot(enc_Br, enc_r) # d2 - encrypted plant calculation
+        enc_x = np.dot(enc_A, enc_x) + np.dot(enc_B, enc_u) # d2 - Only the output of this needs to be encrypted
+        enc_x_vec.append(enc_x.flatten())
+        enc_xr_vec.append(enc_xr.flatten())
+        enc_e = enc_x - enc_xr # d2
+        enc_e_vec.append(enc_e.flatten())
+        #eps = np.dot(enc_c, enc_e)
+
+        x = mat_dec(enc_x, kappa, p, delta)/10
+        e_vec = mat_dec(enc_e_vec, kappa, p, delta)
 
         # Regressor Generator
-        r = 1
-        reg = np.array([[x[1][0]], [r * beta_0 + r_dot * beta_1 + r_ddot - beta_0 * x[0][0] - beta_1 * x[1][0]]])
-        reg_vec.append(reg.flatten())
+        r = -math.sin(t + math.pi / 2) + 1
+        enc_r = enc(r, kappa, p, modulus, delta)
+        enc_p1 = enc_r * enc_beta_0 * enc_d
+        enc_p2 = enc_r_dot * enc_beta_1 * enc_d
+        enc_p3 = enc_r_ddot * enc_d * enc_d
+        enc_p4 = mult(enc_beta_0, enc_x[0][0], modulus)
+        enc_p5 = mult(enc_beta_1, enc_x[1][0], modulus)
+        enc_reg = np.array([[enc_x[1][0]], [enc_p1 + enc_p2 + enc_p3 - enc_p4 - enc_p5]]) #[enc_r * enc_beta_0 * enc_d + enc_r_dot * enc_beta_1 * enc_d + enc_r_ddot * enc_d * enc_d - enc_beta_0 * enc_x[0][0] - enc_beta_1 * enc_x[1][0]]])
+        enc_reg_vec.append(enc_reg.flatten())
+
+        # Debug stuff
+        dec_beta_0 = dec(enc_beta_0, kappa, p, delta)
+        dec_beta_1 = dec(enc_beta_1, kappa, p, delta)
+        dec_xx = dec(enc_x[0][0], kappa, p, delta)
+        p4 = beta_0 * x[0][0]
+        p5 = beta_1 * x[1][0]
+        dec_p4 = dec(enc_p4, kappa, p, delta)
+        dec_p5 = dec(enc_p5, kappa, p, delta)
+
+        reg_vec = mat_dec(enc_reg_vec, kappa, p, delta)
 
         # Parameter adaptation
         par_dot = np.dot(gains, eps * reg)
@@ -102,8 +127,9 @@ def main():
         u = np.dot(reg.transpose(), par)
         u_vec.append(u.flatten())
         k = k + 1
+        t = t + Ts
 
-        t = np.arange(Ts, (k + 1) * Ts, Ts)
+    t = np.arange(Ts, (k + 1) * Ts, Ts)
 
     # Plot the Results
     plt.figure(1)
@@ -126,7 +152,6 @@ def main():
     plt.title('Actual vs Reference Model')
     plt.xlabel('Time')
     plt.show()
-
 
 if __name__ == '__main__':
     main()

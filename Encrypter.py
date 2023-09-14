fixed error calculation

Encrypter.py
@@ -100,7 +100,7 @@ class Encrypter():
 
     def encrypt(s):
         start_time = time.time()
-        iterations = 5000
+        iterations = 2000
         for k in range(1, iterations):
             if s.Encrypt == 2:
                 s.enc_ada(k)
@@ -113,7 +113,8 @@ class Encrypter():
         execution_time = end_time - start_time
         print(f"Execution time: {execution_time/iterations} seconds")
         print(f"State 1 error becomes < {s.e_tol} permanently at iteration {s.ss_k} and time {(s.ss_k/iterations) * s.t[-1]}")
-        print(f"State 1 average error is {np.mean(s.e_vec)}")
+        avg = abs(np.array(s.e_vec)[:, 0])
+        print(f"State 1 average error is {np.mean(avg)}")
 
     def enc_ada(s, k):
         # encryption of matrices and variables
@@ -403,7 +404,7 @@ class Encrypter():
         # Regressor Generator split into parts for debugging
         s.r = (-math.sin(s.t + math.pi / 2) + 1)
         s.r_vec.append(s.r)
-        reg = np.array([[(s.r - s.x[0][0]) * s.beta_2 - s.x[1][0] * s.beta_1],[s.x[1][0]]])
+        reg = np.array([[(s.r - s.x[0][0]) * s.beta_2 - s.x[1][0] * s.beta_1], [s.x[1][0]]])
         s.reg_vec.append(reg.flatten())
 
         par_mult = eps * reg

main.py
@@ -34,7 +34,7 @@ reset_par = 1  # Reset Encryption of par
 """
 
 def main():
-    e = Encrypter(enc_method=2)  # input enc_ada, encode_ada, or ada here. Instantiating the class
+    e = Encrypter(enc_method=0)  # input enc_ada, encode_ada, or ada here. Instantiating the class
     e.encrypt()
 
     # Analyze the data
@@ -48,15 +48,15 @@ def main():
     # Plot the Results
     plt.close('all')
 
-    plt.figure(1)
+    plt.figure(1, figsize=(8, 5))
     plt.subplot(211)
     plt.plot(e.t, np.array(e.e_vec)[:, 0])
     plt.ylabel('Tracking Error')
     # plt.title(f'Gains: gam1={e.gam1}, gam2={e.gam2}')
     plt.subplot(212)
-    plt.plot(e.t, np.array(e.par_vec)[:, 0], label='Par 0: Mass')
-    plt.plot(e.t, np.array(e.par_vec)[:, 1], label='Par 1: Damping')
-    plt.xlabel('Time (s)')
+    plt.plot(e.t, np.array(e.par_vec)[:, 0], label=r'$\hat{m}$')
+    plt.plot(e.t, np.array(e.par_vec)[:, 1], label=r'$\hat{b}$')
+    plt.xlabel('Time(s)')
     plt.ylabel('Parameters')
     plt.legend(loc='center right')
     # plt.subplot(313)
@@ -65,42 +65,43 @@ def main():
     # plt.ylabel('Output')
     plt.savefig('info.png', dpi=300)  # Specify the filename and DPI (dots per inch)
 
-    plt.figure(2)
+    plt.figure(2, figsize=(8, 5))
     plt.plot(e.t, np.array(e.x_vec)[:, 0], label='Plant')
     plt.plot(e.t, np.array(e.xr_vec)[:, 0], label='Reference model')
     # plt.title('Plant vs Reference Model')
-    plt.ylabel('Output: State 1')
-    plt.xlabel('Time (s)')
+    plt.ylabel(r'Output: $x_1 (m)$')
+    plt.xlabel('Time(s)')
     plt.legend(loc='lower right')
     plt.savefig('outputs.png', dpi=300)  # Specify the filename and DPI (dots per inch)
     plt.show()
 
-    # Create a single figure with two subplots
-    plt.figure(3)
-    fig, axs = plt.subplots(2, 1, figsize=(8, 6))
+    if e.Encrypt == 2:
+        # Create a single figure with two subplots
+        plt.figure(3)
+        fig, axs = plt.subplots(2, 1, figsize=(8, 5))
 
-    # First subplot: Encrypted Plant Input
-    enc_u_vec = np.array(e.enc_u_vec)
-    axs[0].plot(e.t, enc_u_vec)
-    # axs[0].set_title('Encrypted Plant Input')
-    axs[0].set_xlabel('Time (s)')
-    axs[0].set_ylabel('Enc(u): Log Scale')
+        # First subplot: Encrypted Plant Input
+        enc_u_vec = np.array(e.enc_u_vec)
+        axs[0].plot(e.t, enc_u_vec)
+        # axs[0].set_title('Encrypted Plant Input')
+        axs[0].set_xlabel('Time(s)')
+        axs[0].set_ylabel('Enc(u): Log Scale')
 
-    # Second subplot: Decrypted Plant Input
-    e.u_vec = np.array(e.u_vec)
-    axs[1].plot(e.t, e.u_vec[:, 0])
-    # axs[1].set_title('Decrypted Plant Input')
-    axs[1].set_xlabel('Time (s)')
-    axs[1].set_ylabel('Dec(u)')
+        # Second subplot: Decrypted Plant Input
+        e.u_vec = np.array(e.u_vec)
+        axs[1].plot(e.t, e.u_vec[:, 0])
+        # axs[1].set_title('Decrypted Plant Input')
+        axs[1].set_xlabel('Time(s)')
+        axs[1].set_ylabel('Dec(u)')
 
-    # Adjust the space between subplots
-    plt.tight_layout()
+        # Adjust the space between subplots
+        plt.tight_layout()
 
-    # Save the combined plot
-    plt.savefig('inputs.png', dpi=300)
+        # Save the combined plot
+        plt.savefig('inputs.png', dpi=300)
 
-    # Show the combined plot
-    plt.show()
+        # Show the combined plot
+        plt.show()
 
 if __name__ == '__main__':
     main()
7d7618319c6f8793c8753415d206a8215b0ed7b6
Jacob Blevins <jacob.blevins@gatech.edu>
2024-05-08 18:42:11 -0400

    Overflow parameter criteria calculations work now!

--- overFlow2.py
+++ overFlow2.py
@@ -1,37 +1,28 @@
 from dyersmat import *
+import math
 
-bit_length = 700
-rho = 16
-rho_ = 100
-delta = .1
+max_num = 134217727
+d = 4  # Degree of polynomial
+n = 1  # Number of variables in polynomial
+
+kappa_ineq = ((n+1)**d)*(max_num**d)
+nu = math.log2(kappa_ineq+1)
+
+bit_length = 900
+rho = 32
+rho_ = math.ceil(nu)+rho
+delta = 1
 kappa, p = keygen(bit_length, rho, rho_)
 mod = pgen(bit_length, rho_, p)
 
-d = 4  # Degree of polynomial
-n = 1  # Number of variables in polynomial
-# 8e32
+p_ineq = ((n+1)**d)*(max_num+(kappa**2))**d
+lam = math.log2(p_ineq+1)
 
 # Equations
 x = 100
-enc_x = enc(x, kappa, p, mod, delta)
-
 u = x**d
+enc_x = enc(x, kappa, p, mod, delta)
 enc_u = mult(mult(mult(enc_x, enc_x, mod), enc_x, mod), enc_x, mod)
-
-
-test1 = ((n+1)**d)*(u**d)
-test2 = ((n+1)**d)*((u+(kappa**2))**d)
-
-if (kappa > test1):
-    print("Condition 1 satisfied")
-else:
-    print("Condition 1 NOT satisfied")
-
-if (p > test2):
-    print("Condition 2 satisfied")
-else:
-    print("Condition 2 NOT satisfied")
-
 dec_u = dec(enc_u, kappa, p, delta**d)
 
 print(u)

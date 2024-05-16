3db781e2b0bdacc88a6ea91f2d02dd6eb7a277b7
Jacob Blevins <jacob.blevins@gatech.edu>
2024-05-15 23:39:00 -0400

    Overflow tuning test code plots and works better

--- overFlow2.py
+++ overFlow2.py
@@ -1,5 +1,7 @@
 from dyersmat import *
 import math
+import matplotlib.pyplot as plt
+from mpmath import mp
 
 M = 100000000 #134217727 # STEP 1 & 2: max number in the algorithm
 d = 4  # Degree of polynomial: x^d
@@ -8,7 +10,7 @@ n = 1  # Number of variables in polynomial
 kappa_min = ((n+1)**d)*(M**d) # STEP 3
 nu = math.log2(kappa_min+1) # STEP 4
 
-bit_length = 890
+bit_length = 888
 rho = 32 # STEP 4 setting rho
 rho_ = math.ceil(nu)+rho # STEP 4 determining rho'
 delta = 1 # using integers so no encoding factor right now
@@ -25,10 +27,11 @@ x = 100
 u = x**d # actual u
 enc_x = enc(x, kappa, p, mod, delta)
 enc_u = mult(mult(mult(enc_x, enc_x, mod), enc_x, mod), enc_x, mod) # Cyphertext operations
+original_u = enc_u
 
 #FDIA: uncomment below to see overflow happen
-# attack = 2
-# enc_u = mult(enc_u, enc(attack, kappa, p, mod, delta), mod)
+attack = 10000
+enc_u = mult(enc_u, attack, mod)
 
 # decrypting final u
 dec_u = dec(enc_u, kappa, p, delta**d) # decrypted u
@@ -41,3 +44,21 @@ if u != dec_u:
     print('FDIA Detected')
 else:
     print('No FDIA Detected')
+
+enc_u = mp.mpf(int(enc_u))
+enc_u = mp.log10(enc_u)
+original_u = mp.mpf(int(original_u))
+original_u = mp.log10(original_u)
+print(enc_u)
+print(original_u)
+
+# Plotting u and M over time
+plt.figure(1, figsize=(8, 4.5))
+plt.plot([0, 1], [original_u, original_u], label='Allowed')  # Plot u
+# plt.plot([0, 1], [enc_u, enc_u], color='r', linestyle='--', label='Attacked')  # Plot M
+# plt.ylim(0, max(u, M)*1.1)  # Set y-axis limit to include 0 and extend slightly beyond max(u, M)
+plt.xlim(0, 1)
+plt.xlabel('Time (s)', fontsize=16)
+plt.ylabel('Plaintext Value', fontsize=16)
+plt.legend()
+plt.show()

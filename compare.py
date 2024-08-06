8ee7e8ddc04792413367fa074ccdc7fa66280381
Jacob Blevins <jacob.blevins@gatech.edu>
2024-08-06 16:05:14 -0400

    Final push before fall 2024 submission. Slowly reworking to object oriented.

--- compare.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-def write_matrices_to_csv(matrices, titles, filename):
-    with open(filename, 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(titles)  # Write header row
-        rows = zip(*matrices)  # Transpose matrices to get rows
-        writer.writerows(rows)
--- testagain.py
+++
@@ -1,6 +0,0 @@
-import math
-import numpy as np
-import matplotlib.pyplot as plt
-from dyersmat import *
-from tuning import *
-
